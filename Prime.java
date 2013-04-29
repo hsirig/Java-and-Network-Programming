614de54a989901a709d22c4c5212344d9184419c
theGeeksHub <girish@hotmail.co.in>
2013-04-29 13:28:59 +0530

    Aligned contents

--- Prime.java
+++ Prime.java
@@ -1,23 +1,23 @@
 import java.io.*;
 class Prime
 { 
-    public static void main(String args[]) throws IOException
-	  {
-		    int number, flag = 0;
-        System.out.print("Enter a number: ");
-		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		    number = Integer.parseInt(br.readLine());
-		    for(int i = 2; i <= (number / 2); i++)
-		    {
-    			  if((number % i) == 0)
-			      {
-    				    flag = 1;		
-				        break;
-			      }
-		    }
-		    if(flag == 0)
-    			  System.out.println(number + " is a prime number.");
-		    else
-			      System.out.println(number + " is not a prime number.");
-	  }
+	public static void main(String args[]) throws IOException
+    	{
+    		int number, flag = 0;
+        	System.out.print("Enter a number: ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		number = Integer.parseInt(br.readLine());
+		for(int i = 2; i <= (number / 2); i++)
+		{
+    			if((number % i) == 0)
+			{
+    				flag = 1;		
+				break;
+			}
+		}
+		if(flag == 0)
+    			System.out.println(number + " is a prime number.");
+		else
+			System.out.println(number + " is not a prime number.");
+	}
 }
